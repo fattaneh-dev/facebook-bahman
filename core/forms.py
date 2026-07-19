4dbf127d7beb1286c30850a034c847a0fc9b4750
fattaneh-python <fataneh.fallah@gmail.com>
2026-07-20 01:14:49 +0330

    add post form

--- core/forms.py
+++ core/forms.py
@@ -3,7 +3,7 @@ from core.models import User
 
 class NewPostForm(forms.Form):
     title = forms.CharField(label='عنوان', widget=forms.TextInput(attrs={'class':'form-control mt-1'}))
-    content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
+    content = forms.CharField(label='محتوا',widget=forms.Textarea(attrs={'class':'form-control'}))
     user = forms.ModelChoiceField(queryset=User.objects.all(), label='کاربر', widget=forms.Select(attrs={'class':'form-select'}))
-    image = forms.ImageField()
+    image = forms.ImageField(label='عکس')
 
--- core/models.py
+++ core/models.py
@@ -8,14 +8,14 @@ class CountryChoices(models.TextChoices):
 
 class User(models.Model):
     username = models.CharField(max_length=50, verbose_name='نام کاربری')
-    password = models.CharField(max_length=20)
-    name = models.CharField(max_length=50)
-    birthdate = models.DateField(null=True)
-    email = models.EmailField()
-    coutry = models.CharField(max_length=20, choices=CountryChoices.choices, default=CountryChoices.IRAN)
-    phone = models.CharField(max_length=11)
-    height = models.PositiveSmallIntegerField(null=True)
-    income = models.DecimalField(max_digits=8, decimal_places=2)
+    password = models.CharField(max_length=20,verbose_name='گذر واژه')
+    name = models.CharField(max_length=50,verbose_name='نام')
+    birthdate = models.DateField(null=True,verbose_name='تاریخ تولد')
+    email = models.EmailField(verbose_name='ایمیل')
+    coutry = models.CharField(max_length=20, choices=CountryChoices.choices, default=CountryChoices.IRAN,verbose_name='نام کاربری')
+    phone = models.CharField(max_length=11,verbose_name='تلفن')
+    height = models.PositiveSmallIntegerField(null=True,verbose_name='قد')
+    income = models.DecimalField(max_digits=8, decimal_places=2,verbose_name='درآمد')
 
     def __str__(self):
         return f'{self.username}:{self.email}'
@@ -24,12 +24,15 @@ class User(models.Model):
         verbose_name_plural = 'کاربران'
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_upate = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='posts/images/', null=True, blank=True)
+    title = models.CharField(max_length=255,verbose_name='عنوان')
+    content = models.TextField(verbose_name='محتوا')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,verbose_name='کاربر')
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name='تاریخ تولید')
+    last_upate = models.DateTimeField(auto_now=True,verbose_name='تاریخ ویرایش')
+    image = models.ImageField(upload_to='posts/images/', null=True, blank=True,verbose_name='عکس')
 
     def __str__(self):
         return self.title
+    class Meta:
+            verbose_name = 'پست'
+            verbose_name_plural = 'پستها'
