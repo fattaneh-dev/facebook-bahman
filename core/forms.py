9ff1b297d968190929e786ff24578c76d514b6ad
fattaneh-python <fataneh.fallah@gmail.com>
2026-07-22 00:13:43 +0330

    update forms.py

--- core/forms.py
+++ core/forms.py
@@ -4,7 +4,7 @@ from core.models import User
 
 class NewPostForm(forms.Form):
     title = forms.CharField(label='عنوان', widget=forms.TextInput(attrs={'class':'form-control mt-1'}))
-    content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
+    content = forms.CharField(label='عنوان',widget=forms.Textarea(attrs={'class':'form-control'}))
     user = forms.ModelChoiceField(queryset=User.objects.all(), label='کاربر', widget=forms.Select(attrs={'class':'form-select'}))
-    image = forms.ImageField()
+    image = forms.ImageField(label='عکس')
 
